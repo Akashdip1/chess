0c88aeda84434ebd50afbeed7b837ef2cfe9fd8d
Akashdip1 <akashdip.baral@gmail.com>
2024-01-23 20:27:29 +0530

    added a simple boilerplate

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import pygame
 
+pygame.init()
+
+WIDTH, HEIGHT = 1600, 1600
+
+FPS = 60
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        screen.fill("white")
+        pygame.display.update()
+        
+        clock.tick(FPS)
+    pygame.quit()
+       
+if __name__ == "__main__":
+    main()
