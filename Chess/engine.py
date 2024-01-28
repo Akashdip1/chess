b54d86b9dc5a018e617929fababfca8076e3b63b
Akashdip1 <akashdip.baral@gmail.com>
2024-01-28 19:34:01 +0530

    Added functionality to move a piece in the board

--- Chess/engine.py
+++ Chess/engine.py
@@ -21,5 +21,29 @@ class GameState():
         
         self.whiteToMove = True
         self.moveLog = []
-        
+       
+    def makeMove(self, move): # move = instance of a Move class
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move) #log of the moves
+        self.whiteToMove = not self.whiteToMove # Switch turns of players
+ 
+class Move():
+    ranksToRows = {"1" : 7, "2" : 6, "3" : 5,"4" : 4, "5" : 3, "6" : 2, "7" : 1, "8" : 0}
+    rowsToRanks = {v : k for k, v in ranksToRows.items()} 
+    filesToCols = {"a" : 0, "b" : 1, "c" : 2, "d" : 3,"e" : 4, "f" : 5, "g" : 6, "h" : 7}
+    colsToFiles = {v : k for k, v in filesToCols.items()} 
 
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol] 
+        self.pieceCaptured = board[self.endRow][self.endCol]
+    
+    def getChessNotation(self):
+        return self.getFileRank(self.startCol, self.startRow) + self.getFileRank(self.endCol, self.endRow)
+        
+    def getFileRank(self, c, r):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
--- Chess/main.py
+++ Chess/main.py
@@ -5,6 +5,7 @@ object
 
 import pygame as p
 import engine
+import pprint
 
 p.init()
 WIDTH = HEIGHT = 800
@@ -29,34 +30,30 @@ def loadImages():
 
 # drawBoard() will draw the squares on the board
 def drawBoard(screen):
+    colors = [LIGHT, DARK]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
-            if (r + c) % 2 == 1:
-                p.draw.rect(
-                    screen,
-                    DARK,
-                    p.Rect(c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE),
-                )
-
+            color = colors[(r+c) % 2]
+            p.draw.rect(screen, color, p.Rect(c*SQUARE_SIZE, r*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
 # drawPieces() will draw the pieces on the board using the current GameState.board
 def drawPieces(screen, board):
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             if board[r][c] != "--":
-                piece = IMAGES[board[r][c]]
-                screen.blit(piece, (SQUARE_SIZE * c, SQUARE_SIZE * r))
-            else:
-                continue
+                piece = board[r][c]
+                screen.blit(IMAGES[piece], (SQUARE_SIZE * c, SQUARE_SIZE * r))
 
 
 def drawGameState(screen, gs):
     drawBoard(screen)
     drawPieces(screen, gs.board)
-    
+
+
 """
 This will be our main driver which will be responsible for user input and updating the graphics 
 """
+
 
 def main():
     screen = p.display.set_mode((WIDTH, HEIGHT))
@@ -68,14 +65,34 @@ def main():
     drawGameState(screen, gs)
     p.display.update()
 
+    sqSelected = ()  # To keep track of the squares selected by the user, (tuple, (row, col))
+    playerClicks = []  # To keep track of the first and last clicked squares,[(6, 4), (4, 4)]
+
     running = True
     while running:
         for event in p.event.get():
             if event.type == p.QUIT:
                 running = False
 
+            elif event.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # location of the mouse in tuple (x, y)
+                col = location[0] // SQUARE_SIZE
+                row = location[1] // SQUARE_SIZE  # row and column of the square clicked
+                if sqSelected == (row, col):
+                    sqSelected, playerClicks = (), []
+                      # deselect if player clicks same square twice
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)  # append for both 1st and 2nd click
+                if len(playerClicks) == 2:  # move the chess piece after 2 clicks
+                    move = engine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    gs.makeMove(move)
+                    sqSelected = ()
+                    playerClicks = [] # reset
+
+        drawGameState(screen, gs)
+        clock.tick(FPS)
+        p.display.flip()
 
 if __name__ == "__main__":
     main()
-
-# To see git changes
